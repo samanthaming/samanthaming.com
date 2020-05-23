2afd7e0c506b71772e06e450395820aa32e00fd2
Samantha Ming <sam@samanthaming.com>
2020-05-23 13:10:04 -0700

    Accepting translations ✨

---
+++ articles/flexbox30/TRANSLATION.js
@@ -0,0 +1,98 @@
+// =================================================================
+// Please read the "Translation Contribution Guide" before starting
+// =================================================================
+module.exports = {
+  'flexbox30/1-flexbox-intro': {
+
+  },
+  'flexbox30/2-flex-container-and-flex-items': {
+
+  },
+  'flexbox30/3-immediate-child-only': {
+
+  },
+  'flexbox30/4-flexbox-axes': {
+
+  },
+  'flexbox30/5-flexbox-module': {
+
+  },
+  'flexbox30/6-parent-properties': {
+
+  },
+  'flexbox30/7-display': {
+
+  },
+  'flexbox30/8-block-vs-inline': {
+
+  },
+  'flexbox30/9-flex-direction': {
+
+  },
+  'flexbox30/10-flex-wrap': {
+
+  },
+  'flexbox30/11-flex-flow': {
+
+  },
+  'flexbox30/12-justify-content-row': {
+
+  },
+  'flexbox30/13-space-around-vs-space-evenly': {
+
+  },
+  'flexbox30/14-justify-content-column': {
+
+  },
+  'flexbox30/15-align-items-row': {
+
+  },
+  'flexbox30/16-baseline': {
+
+  },
+  'flexbox30/17-align-items-column': {
+
+  },
+  'flexbox30/18-align-content': {
+
+  },
+  'flexbox30/19-child-properties': {
+
+  },
+  'flexbox30/20-order': {
+
+  },
+  'flexbox30/21-flex-grow': {
+
+  },
+  'flexbox30/22-flex-grow-calculation': {
+
+  },
+  'flexbox30/23-flex-shrink': {
+
+  },
+  'flexbox30/24-flex-shrink-calculation': {
+
+  },
+  'flexbox30/25-flex-basis': {
+
+  },
+  'flexbox30/26-flex-basis-vs-widths': {
+
+  },
+  'flexbox30/27-flex': {
+
+  },
+  'flexbox30/28-align-self': {
+
+  },
+  'flexbox30/29-flexbox-properties': {
+
+  },
+  'flexbox30/30-flexbox-cheatsheet': {
+
+  },
+  'flexbox30/31-flexbox-with-auto-margins': {
+
+  },
+}
---
+++ articles/tidbits/TRANSLATION.js
@@ -0,0 +1,297 @@
+/** Please read the "Translation Contribution Guide" before starting
+
+module.exports = {
+  'tidbits/1-convert-array-like-to-true-array': {
+    'es': {             // 👈 Language Code
+      'file': '',       // 👈 Your File name
+      'title': '',      // 👈 Meta Title
+      'description': '' // 👈 OPTIONAL: Meta Description (max 155 characters)
+    }
+  }
+}
+========
+EXAMPLE
+========
+module.exports = {
+  'tidbits/1-convert-array-like-to-true-array': {
+    'pt': {
+      'file': '1-convertendo-um-array-like-para-um-array',
+      'title': 'Convertendo um Array-Like para um Array',
+      'description': ''
+    },
+    'zh-tw': {
+      'file': '1-convert-array-like-to-true-array',
+      title: '將數組轉換為真數組'
+    },
+  }
+}
+*/
+module.exports = {
+  'tidbits/1-convert-array-like-to-true-array': {
+
+  },
+  'tidbits/2-console-table': {
+
+  },
+  'tidbits/3-when-not-to-use-arrow-functions': {
+
+  },
+  'tidbits/4-declaring-css-variables': {
+
+  },
+  'tidbits/5-concise-method-syntax': {
+
+  },
+  'tidbits/6-custom-css-selection-styling': {
+
+  },
+  'tidbits/7-create-object-with-dynamic-keys': {
+
+  },
+  'tidbits/8-swap-variables-with-destructuring': {
+
+  },
+  'tidbits/9-trick-to-add-string-and-number': {
+
+  },
+  'tidbits/10-remove-array-duplicates-using-set': {
+
+  },
+  'tidbits/11-setting-default-parameters': {
+
+  },
+  'tidbits/12-split-string-using-spread': {
+
+  },
+  'tidbits/13-skip-values-in-destructuring': {
+
+  },
+  'tidbits/14-combine-multiple-arrays-using-spread': {
+
+  },
+  'tidbits/15-4-ways-to-combine-strings': {
+
+  },
+  'tidbits/16-default-parameters': {
+
+  },
+  'tidbits/17-print-ranges-natively': {
+
+  },
+  'tidbits/18-named-param': {
+
+  },
+  'tidbits/19-2-ways-to-convert-to-boolean': {
+
+  },
+  'tidbits/20-destructuring-function-arguments': {
+
+  },
+  'tidbits/21-how-to-test-nan': {
+
+  },
+  'tidbits/22-2-ways-to-repeat-strings': {
+
+  },
+  'tidbits/23-no-else-return': {
+
+  },
+  'tidbits/24-prevent-error-with-default-object-when-destructuring': {
+
+  },
+  'tidbits/25-js-essentials-falsy-values': {
+
+  },
+  'tidbits/26-refactoring-if-condition': {
+
+  },
+  'tidbits/27-css-user-select': {
+
+  },
+  'tidbits/28-check-if-string-contains-substring': {
+
+  },
+  'tidbits/29-check-if-number-is-positive-or-negative': {
+
+  },
+  'tidbits/30-how-to-format-current-currency-in-es6': {
+
+  },
+  'tidbits/31-css-caret-color': {
+
+  },
+  'tidbits/32-html-contenteditable': {
+
+  },
+  'tidbits/33-how-to-compare-2-objects': {
+
+  },
+  'tidbits/34-better-boolean-variable-names': {
+
+  },
+  'tidbits/35-es6-way-to-clone-an-array': {
+
+  },
+  'tidbits/36-bad-variable-names-to-avoid': {
+
+  },
+  'tidbits/37-dynamic-property-name-with-es6': {
+
+  },
+  'tidbits/38-html-download-attribute': {
+
+  },
+  'tidbits/39-es6-classes': {
+
+  },
+  'tidbits/40-colorful-console-message': {
+
+  },
+  'tidbits/41-no-useless-escape': {
+
+  },
+  'tidbits/42-html5-autofocus': {
+
+  },
+  'tidbits/43-3-ways-to-remove-array-duplicates': {
+
+  },
+  'tidbits/44-refactoring-conditional-adding-property-to-object': {
+
+  },
+  'tidbits/45-pretty-json-ouptput': {
+
+  },
+  'tidbits/46-css-not-selector': {
+
+  },
+  'tidbits/47-arrow-functions-cheatsheet': {
+
+  },
+  'tidbits/48-passing-arrays-as-function-arguments': {
+
+  },
+  'tidbits/49-2-ways-to-merge-arrays': {
+
+  },
+  'tidbits/50-how-to-deep-clone-an-array': {
+
+  },
+  'tidbits/51-prevent-object-retrieval-type-error-with-guard-operator': {
+
+  },
+  'tidbits/52-3-ways-to-set-default-value': {
+
+  },
+  'tidbits/53-css-counter': {
+
+  },
+  'tidbits/54-quick-debug-with-console-log-using-or-operator': {
+
+  },
+  'tidbits/55-how-to-truncate-number': {
+
+  },
+  'tidbits/56-how-to-get-an-object-length': {
+
+  },
+  'tidbits/57-styling-css-placeholder': {
+
+  },
+  'tidbits/58-multi-line-strings-with-template-literals': {
+
+  },
+  'tidbits/59-exponentiation-operator': {
+
+  },
+  'tidbits/60-highlight-text-with-html-mark-tag': {
+
+  },
+  'tidbits/61-better-nan-check-with-es6-number-isnan': {
+
+  },
+  'tidbits/62-5-ways-to-convert-value-to-string': {
+
+  },
+  'tidbits/63-better-array-check-with-array-isarray': {
+
+  },
+  'tidbits/64-css-only-child': {
+
+  },
+  'tidbits/65-dot-vs-bracket-notation': {
+
+  },
+  'tidbits/66-no-and-in-function-name': {
+
+  },
+  'tidbits/67-es6-startswith-method': {
+
+  },
+  'tidbits/68-strike-through-using-html': {
+
+  },
+  'tidbits/69-display-string-in-tabular-format-with-padend': {
+
+  },
+  'tidbits/70-3-ways-to-clone-objects': {
+
+  },
+  'tidbits/71-how-to-flatten-array-using-array-flat': {
+
+  },
+  'tidbits/72-css-empty-selector': {
+
+  },
+  'tidbits/73-es6-endswith-method': {
+
+  },
+  'tidbits/74-how-to-reverse-an-array': {
+
+  },
+  'tidbits/75-centering-elements-with-flexbox': {
+
+  },
+  'tidbits/76-converting-object-to-array': {
+
+  },
+  'tidbits/77-html-kbd-tag': {
+
+  },
+  'tidbits/78-iterables-to-array-using-spread': {
+
+  },
+  'tidbits/79-module-cheatsheet': {
+
+  },
+  'tidbits/80-flatmap': {
+
+  },
+  'tidbits/81-how-to-check-if-array-includes-a-value': {
+
+  },
+  'tidbits/82-html-audio-tag': {
+
+  },
+  'tidbits/83-4-ways-to-convert-string-to-character-array': {
+
+  },
+  'tidbits/84-2-ways-to-center-elements-with-flexbox': {
+
+  },
+  'tidbits/85-string-trim': {
+
+  },
+  'tidbits/86-window-location-cheatsheet': {
+
+  },
+  'tidbits/87-5-ways-to-append-item-to-array': {
+
+  },
+  'tidbits/88-css-placeholder-shown': {
+
+  },
+  'tidbits/89-how-to-check-if-variable-is-array': {
+
+  },
+}
