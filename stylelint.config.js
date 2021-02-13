46b66c3d76040782f3d1453f4e4d533a4b746cc8
Samantha Ming <sam@samanthaming.com>
2021-02-13 09:35:57 -0800

    add stylelint-scss

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  plugins: ['stylelint-scss'],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {},
-}
+  rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+  },
+};
