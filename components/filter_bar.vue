52e83cd2295ad426f2478d61978f0f53b606eefa
Samantha Ming <sam@samanthaming.com>
2021-06-21 12:28:53 -0700

    make filter bar opaque

--- components/filter_bar.vue
+++ components/filter_bar.vue
@@ -1,12 +1,12 @@
 <template>
   <div
-    class="px-2 py-4 lg:pt-5 lg:pb-4 border-b shadow-md flex justify-center font-head space-x-6 md:space-x-7 lg:space-x-5"
+    class="px-2 py-4 lg:pt-5 lg:pb-4 border-b shadow-lg flex justify-center font-head space-x-6 md:space-x-7 lg:space-x-5"
     :class="[colorOption.background, colorOption.border]"
   >
     <button
       v-for="({ value, text }, index) in filterOptions"
       :key="index"
-      class="inline-flex items-center justify-center w-24 sm:w-28 md:w-32 py-2 border text-xs sm:text-sm md:text-base font-medium rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 hover:text-ink"
+      class="inline-flex items-center justify-center w-24 sm:w-28 md:w-32 py-1.5 border-2 text-xs sm:text-sm md:text-base font-semibold rounded-full shadow-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 hover:text-ink"
       :class="value === activeButton ? activeButtonClass : inactiveButtonClass"
       :disabled="value === activeButton"
       @click="onClick(value)"
@@ -22,15 +22,15 @@ import { TAG_JS, TAG_CSS, TAG_PROGRAMMING, TAG_CAREER } from '~/lib';
 const COLOR_OPTION = {
   orange: {
     border: 'border-orange-light',
-    background: 'bg-orange-lighter',
-    text: 'text-orange-darker',
+    background: 'bg-orange-lighter bg-opacity-40',
+    text: 'text-orange-darkest',
     buttonBackground: 'bg-orange-white hover:bg-orange-lightest',
     buttonBorder: 'border-orange-light hover:border-orange',
-    active: 'bg-orange border-orange',
+    active: 'bg-orange border-orange text-orange-black',
   },
   green: {
     border: 'border-green-light',
-    background: 'bg-green-lightest',
+    background: 'bg-green-lightest bg-opacity-50',
     text: 'text-green-dark',
     buttonBackground: 'bg-white hover:bg-green-lighter',
     buttonBorder: 'border-green-light hover:border-green',
